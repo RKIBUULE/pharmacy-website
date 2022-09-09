4c306dbc54a8463dfa8733cf1881059cfaa53cb9
RKIBUULE <kibuulerogers58@gmail.com>
2022-09-09 15:19:47 +0300

    commit

---
+++ src/Pages/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+      <div className='nav__container'>
+        <div className='nav'>
+            <div className='company__logo'>
+                COMPANY NAME
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li></li>
+            </ul>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Landing
